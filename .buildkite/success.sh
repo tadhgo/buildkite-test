2970be829fa4ab6d2ef54f6b180a3afca47bf782
Tadhg OBrien <tadhgobrien10@gmail.com>
2024-11-11 21:06:01 -0500

    implemented wait for success.sh script

---
+++ .buildkite/success.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+printf '\033]1338;url=https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbzg2cnd0MG1vcmVycmQ0djE0bDBmNzY2anRwazB3aDN0cTQzZDJmMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MFxVGhp8NlrywjZNoK/giphy.gif;alt=success\a\n'
